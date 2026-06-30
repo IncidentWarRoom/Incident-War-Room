9463ae7f163979634dc64ce9917c7c46d4e40840
Thunder22020 <d.shchipanov@innopolis.university>
2026-06-30 11:33:19 +0300

    Revert "Merge pull request #37 from IncidentWarRoom/frontend"
    This reverts commit 3d97319a22b6af6b19e00fbbf6633745ce796de7, reversing
    changes made to 3325b46865bd180fcfa984c3b64aba950738ef11.

--- EmptyState.jsx
+++
@@ -1,3 +0,0 @@
-export default function EmptyState({ message }) {
-  return <div className="wr-empty">{message}</div>;
-}
--- Skeleton.jsx
+++
@@ -1,8 +0,0 @@
-export default function Skeleton({ width = '100%', height = 14, style = {} }) {
-  return (
-    <div
-      className="wr-skel"
-      style={{ width, height, ...style }}
-    />
-  );
-}
